b1c6c9b5ed9fae4bc8f679d94e9f25f867aa62dd
Ahanaf Abrar <100956005+Ahanaf-Abrar-Trishad@users.noreply.github.com>
2024-08-12 23:28:36 +0600

    12th Aug

---
+++ assets/js/global.js
@@ -0,0 +1,21 @@
+/**
+ * @license MIT
+ * @copyright 2023 codewithsadee
+ * @author codewithsadee <mohammadsadee24@gmail.com>
+ */
+
+"use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements 
+ * @param {String} eventType Event type string 
+ * @param {Function} callback Callback function
+ */
+
+
+window.addEventOnElements = ($elements, eventType, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback);
+    }
+}
